1492965a0ecd3f72b462c7979cccb5d686869046
Brandon Shin <brandonshin26@gmail.com>
2026-02-18 21:44:20 -0800

    Wrote code for Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,3 +1,30 @@
 public class Transaction {
-    
+    // Fields
+    private int month;
+    private int day;
+    private double amount;
+
+    // Constructors
+    public Transaction(int myMonth, int myDay, int myAmount){
+        month = myMonth;
+        day = myDay;
+        amount = myAmount;
+    }
+
+    // Methods
+    public int getMonth(){
+        return month;
+    }
+
+    public int getDay(){
+        return day;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public String toString(){
+        return month + "/" + day + ": $" + amount;
+    }
 }
